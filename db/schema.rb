36aadcfbb2ed9bcaaa3ae1ba15efea653cbc3164
Ta Minh Tien <taminhtien1993@gmail.com>
2015-07-23 21:07:10 +0700

    Add name to devise Userr

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150723121204) do
+ActiveRecord::Schema.define(version: 20150723124213) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "title"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20150723121204) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
