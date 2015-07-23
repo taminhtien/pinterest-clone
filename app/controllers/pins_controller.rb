0e99c90567da6aa31e78a71441cd6fbde7e2afd8
Ta Minh Tien <taminhtien1993@gmail.com>
2015-07-23 22:50:14 +0700

    Add and configure acts_as_votable

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,5 @@
 class PinsController < ApplicationController
-  before_action :find_pin, only: [:show, :edit, :update, :destroy]
+  before_action :find_pin, only: [:show, :edit, :update, :destroy, :upvote]
   def index
     @pins = Pin.all
   end
@@ -34,6 +34,11 @@ class PinsController < ApplicationController
   def destroy
     @pin.destroy
     redirect_to root_path, notice: "Successfully deleted pin"
+  end
+
+  def upvote
+    @pin.upvote_by current_user
+    redirect_to :back
   end
 
   private
